e9b2cc270d66dc84cd5c017421fdfcc0ad3c5061
Miguel Ángel Suvires <suvires@gmail.com>
2024-01-06 01:06:16 +0100

    Final

--- src/app/page.js
+++ src/app/page.js
@@ -41,7 +41,7 @@ export default function Home() {
       <main>
         <div className="step">
           <h1>¡Oh!</h1>
-          <p>Haremos como que no hemos visto nada... ¡Podéis volver a intentarlo!</p>
+          <p>Haremos como que no hemos visto nada... ¡Puedes volver a intentarlo!</p>
           <button className="retry" onClick={() => setError(false)}>Reintentar</button>
         </div>
       </main>
@@ -61,9 +61,8 @@ export default function Home() {
         }
         <main>
           <div className="step">
-            <h1>Madrid desde las alturas en la mejor terraza</h1>
-            <p>Vale por una visita al 360º Rooftop Bar del Hotel Riu Madrid Plaza España.</p>
-            <p>La visita incluye la subida a la terraza y disfrutar de las impactantes vistas con una consumición.</p>
+            <h1>Recorre Mallorca para conocer los mejores rincones de la isla</h1>
+            <p>Vale por el alquiler de un coche durante 4-5 días de tus vacaciones en Mallorca.</p>
             <button onClick={() => { setRight(false); setStep(0)}}>Reiniciar</button>
           </div>
         </main>
@@ -76,7 +75,7 @@ export default function Home() {
       <main>
         <div className="step">
           <h1>¡Genial!</h1>
-          <p>Estáis un pasito más cerca de recibir vuestro regalo.</p>
+          <p>Estás un pasito más cerca de recibir tu regalo.</p>
           <button className="retry" onClick={() => { setRight(false); setStep(step+1)}}>Continuar</button>
         </div>
       </main>
@@ -88,39 +87,39 @@ export default function Home() {
       {step === 0 && (
         <div className="step">
           <h1>¡Han venido los Reyes!</h1>
-          <p>A continuación, cada uno de SS.MM. os vamos a hacer 3 preguntas.</p>
-          <p>Si las acertáis todas, ¡recibiréis vuestro regalo! De lo contrario: carbón...</p>
-          <p>¿Estáis preparados?</p>
+          <p>A continuación, cada uno de SS.MM. te hará 3 preguntas.</p>
+          <p>Si las aciertas todas, ¡recibirás tu regalo! De lo contrario: carbón...</p>
+          <p>¿Estás preparada?</p>
           <button className="start" onClick={() => setStep(step + 1)}>Comenzar</button>
         </div>
       )}
       {step === 1 && (
         <div className="step">
-          <h1>Ramillete de jazmines en forma de bola</h1>
+          <h1>Bollo típico de Mallorca</h1>
           <ul>
-            <li onClick={() => setError(true)}>Espeto</li>
-            <li onClick={() => setRight(true)}>Biznaga</li>
-            <li onClick={() => setError(true)}>Cenachero</li>
+            <li onClick={() => setError(true)}>Bollet</li>
+            <li onClick={() => setError(true)}>Mallorquina</li>
+            <li onClick={() => setRight(true)}>Ensaimada</li>
           </ul>
         </div>
       )}
       {step === 2 && (
         <div className="step">
-          <h1>Mítico delantero malagueño del Real Madrid</h1>
+          <h1>Estilo arquitectónico de la catedral de Palma</h1>
           <ul>
-            <li onClick={() => setError(true)}>Jaimito</li>
-            <li onClick={() => setError(true)}>Jorgito</li>
-            <li onClick={() => setRight(true)}>Juanito</li>
+            <li onClick={() => setError(true)}>Románico</li>
+            <li onClick={() => setRight(true)}>Gótico</li>
+            <li onClick={() => setError(true)}>Barroco</li>
           </ul>
         </div>
       )}
       {step === 3 && (
         <div className="step">
-          <h1>Típica sopa malagueña hecha con mayonesa</h1>
+          <h1>Comida típica de Mallorca formada por varias tapas en un mismo plato</h1>
           <ul>
-            <li onClick={() => setError(true)}>Pipirrana</li>
-            <li onClick={() => setRight(true) }>Gazpachuelo</li>
-            <li onClick={() => setError(true)}>Porra</li>
+            <li onClick={() => setError(true)}>Revueltó</li>
+            <li onClick={() => setRight(true) }>Variat</li>
+            <li onClick={() => setError(true)}>Meclat</li>
           </ul>
         </div>
       )}
